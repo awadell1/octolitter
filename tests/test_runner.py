f1f3175d6153dcce62ae0184cb6ce5088588ae57
Alexius Wadell <awadell@gmail.com>
2021-01-25 11:39:29 -0800

    Uses GITHUB_TOKEN for testing

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,10 +5,10 @@ from octolitter.repo import GithubRepo
 from octolitter.runner import Runner
 
 
-GITHUB_TOKEN = environ["GITHUB_TOKEN"]
+GITHUB_API = environ["GITHUB_API"]
 
 def test_install():
-    repo = GithubRepo(GITHUB_TOKEN, "https://github.com/awadell1/octolitter")
+    repo = GithubRepo(GITHUB_API, "https://github.com/awadell1/octolitter")
     runner = Runner(repo)
     runner_exe = runner.get_runner_app()
     assert Path(runner_exe).exists()
