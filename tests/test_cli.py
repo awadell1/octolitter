be40319822889b85a37a4108d00e79188dc634e1
Alexius Wadell <awadell@gmail.com>
2021-01-25 14:54:30 -0800

    Test adding/ removing runners

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,14 @@
 from io import StringIO
 import re
 import pytest
+from io import StringIO
+from os import environ
 from contextlib import redirect_stderr, redirect_stdout
 from subprocess import call
 from octolitter.cli import cli
+
+GITHUB_API = environ["GITHUB_API"]
+
 
 def call_octolitter(args=[]):
     """ Check that help message is displayed """
@@ -31,3 +36,15 @@ def test_subcmd_help():
         s, e = call_octolitter_error([subcmd])
         assert e.value.code == 2
         assert re.match(f"usage: octolitter {subcmd}.*", s)
+
+
+def test_add_runner():
+    # Add the runner
+    s = call_octolitter(
+        ["add", "--api", GITHUB_API, "https://github.com/awadell1/octolitter"]
+    )
+    assert re.search("Error", s, re.IGNORECASE) == None
+
+    # Remove the runner
+    s = call_octolitter(["rm", "--api", GITHUB_API, "--all"])
+    assert re.search("Error", s, re.IGNORECASE) == None
